138517bdfa0d0e3751b99364572485a7cd559c33
afu <yulifu@douyu.tv>
2022-09-25 20:25:59 +0800

    add demo

--- app/controllers/index/IndexController.ts
+++ app/controllers/index/IndexController.ts
@@ -2,6 +2,20 @@ import Controller from 'candyjs/web/Controller';
 
 export default class IndexController extends Controller {
 
+    public behaviors(): any[] {
+        return [
+            [
+                'hostCheck',
+                {
+                    classPath: 'app/filters/HostCheck',
+                    allowedHosts: [
+                        '127.0.0.1'
+                    ]
+                }
+            ]
+        ];
+    }
+
     run(req, res) {
         this.getView().getViewContent('index', (err, data) => {
             res.write(data);
