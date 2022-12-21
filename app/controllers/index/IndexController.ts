6b4ffb6e283f3138589d9ec78d15c9f6cfb7c9cd
afu <yulifu@douyu.tv>
2022-12-21 17:01:26 +0800

    rm filter

--- app/controllers/index/IndexController.ts
+++ app/controllers/index/IndexController.ts
@@ -2,20 +2,6 @@ import Controller from 'candyjs/web/Controller';
 
 export default class IndexController extends Controller {
 
-    public behaviors(): any[] {
-        return [
-            [
-                'hostCheck',
-                {
-                    classPath: 'app/filters/HostCheck',
-                    allowedHosts: [
-                        '127.0.0.1'
-                    ]
-                }
-            ]
-        ];
-    }
-
     run(req, res) {
         this.getView().title = 'home page';
         this.getView().enableLayout = true;
